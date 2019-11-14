c4387bf2bd789054fda5923487824a338d90b9c0
Kirill Lastochkin <ranono.zoro@mail.ru>
2019-11-14 13:56:49 +0300

    Update MainWindow.xaml.cs
    Test

--- LaboratoryWork1/MainWindow.xaml.cs
+++ LaboratoryWork1/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ namespace LaboratoryWork1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+	///
     public partial class MainWindow : Window
     {
         public MainWindow()
